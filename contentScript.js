9d8f46fb66df37830b1d5a5049cade15b7deb697
SauravKumarLal <sauravkrlal@gmail.com>
2023-11-01 19:13:33 +0530

    changes

--- contentScript.js
+++ contentScript.js
@@ -1,47 +1,118 @@
-let scrapeData = document.getElementById('scrapeData');
-let list = document.getElementById('dataList');
+// let scrapeData = document.getElementById('scrapeData');
+// let list = document.getElementById('dataList');
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let data = request.data;
 
-    if (data == null || data.length === 0) {
-        let li = document.createElement('li');
-        li.innerText = "No Data Found On This Page";
-        list.appendChild(li);
-    } else {
-        data.forEach((item) => {
-            let li = document.createElement('li');
-            li.innerText = item;
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     let data = request.data;
+
+//     if (data == null || data.length === 0) {
+//         let li = document.createElement('li');
+//         li.innerText = "No Data Found On This Page";
+//         list.appendChild(li);
+//     } else {
+//         data.forEach((item) => {
+//             let li = document.createElement('li');
+//             li.innerText = item;
+//             list.appendChild(li);
+//         });
+//     }
+// });
+
+// scrapeData.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//     chrome.scripting.executeScript({
+//         target: { tabId: tab.id },
+//         func: scrapeDataFromPage,
+//     });
+// });
+
+// function scrapeDataFromPage() {
+//     const titleElements = document.querySelectorAll('h2');
+//     const phoneRegEx = /0\d{10}/gim;
+//     // /(\+\d{1,2}\s?)?(\d{3}[-\s]?\d{3}[-\s]?\d{4}|\(\d{3}\)\s?\d{3}[-\s]?\d{4}|\d{10})/g;
+
+//     let titles = Array.from(titleElements, element => element.innerText);
+//     let phoneMatches = document.body.innerText.match(phoneRegEx);
+
+//     let data = [];
+
+//     if (titles && phoneMatches) {
+//         for (let i = 0; i < titles.length && i < phoneMatches.length; i++) {
+//             data.push(titles[i]);
+//             data.push(phoneMatches[i]);
+//         }
+//     }
+
+//     chrome.runtime.sendMessage({ data });
+// }
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let scrapeData = document.getElementById("scrapeData");
+    let list = document.getElementById("dataList");
+
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        let data = request.data;
+
+        if (data == null || data.length === 0) {
+            let li = document.createElement("li");
+            li.innerText = "No Data Found On This Page";
             list.appendChild(li);
-        });
-    }
-});
-
-scrapeData.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: scrapeDataFromPage,
-    });
-});
-
-function scrapeDataFromPage() {
-    const titleElements = document.querySelectorAll('h2');
-    const phoneRegEx = /0\d{10}/gim;
-    // /(\+\d{1,2}\s?)?(\d{3}[-\s]?\d{3}[-\s]?\d{4}|\(\d{3}\)\s?\d{3}[-\s]?\d{4}|\d{10})/g;
-    
-    let titles = Array.from(titleElements, element => element.innerText);
-    let phoneMatches = document.body.innerText.match(phoneRegEx);
-
-    let data = [];
-
-    if (titles && phoneMatches) {
-        for (let i = 0; i < titles.length && i < phoneMatches.length; i++) {
-            data.push(titles[i]);
-            data.push(phoneMatches[i]);
+        } else {
+            for (const val of data) {
+                for (const key in val) {
+                    let li = document.createElement("li");
+                    li.innerText = val[key];
+                    list.appendChild(li);
+                }
+            }
         }
-    }
 
-    chrome.runtime.sendMessage({ data });
-}
+        const rows = data;
+        const worksheet = XLSX.utils.json_to_sheet(rows);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Just Dail Data");
+
+        XLSX.utils.sheet_add_aoa(worksheet, [["Title", "Phone Number"]], {
+            origin: "A1",
+        });
+
+        const max_width = rows.reduce((w, r) => Math.max(w, r.name.length), 10);
+        worksheet["!cols"] = [{ wch: max_width }, { wch: 20 }];
+
+        XLSX.writeFileXLSX(workbook, "just_dail_data.xlsx");
+    });
+
+    scrapeData.addEventListener("click", async () => {
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: scrapeDataFromPage,
+        });
+    });
+
+    function scrapeDataFromPage() {
+        const titleElements = document.querySelectorAll("h2");
+        const phoneRegEx = /0\d{10}/gim;
+        // /(\+\d{1,2}\s?)?(\d{3}[-\s]?\d{3}[-\s]?\d{4}|\(\d{3}\)\s?\d{3}[-\s]?\d{4}|\d{10})/g;
+
+        let titles = Array.from(titleElements, (element) => element.innerText);
+        let phoneMatches = document.body.innerText.match(phoneRegEx);
+
+        let data = [];
+
+        if (titles && phoneMatches) {
+            for (let i = 0; i < titles.length && i < phoneMatches.length; i++) {
+                const obj = new Object();
+                obj.name = titles[i];
+                obj.phone = phoneMatches[i];
+                data.push(obj);
+            }
+        }
+
+        chrome.runtime.sendMessage({ data });
+    }
+});
